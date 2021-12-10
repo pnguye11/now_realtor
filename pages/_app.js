5171f6aabcdbbced547c643da68de7aa383bae4c
pnguye11 <pnguye11@gmail.com>
2021-12-09 18:16:37 -0800

    individual property display

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,14 @@ import nProgress from "nprogress";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Layout from "../components/Layout";
+import nprogress from "nprogress";
 
 function MyApp({ Component, pageProps }) {
+  nProgress.configure({ showSpinner: false });
+
+  Router.events.on("routeChangeStart", () => {
+    nprogress.start();
+  });
   return (
     <>
       <Head />
