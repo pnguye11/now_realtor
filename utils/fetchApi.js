3ed7a5a7529ef3ca7723efafe7f2d6f2cf85d65b
pnguye11 <pnguye11@gmail.com>
2022-01-14 11:54:35 -0800

    remove api

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       "x-rapidapi-host": "bayut.p.rapidapi.com",
-      "x-rapidapi-key": "567fccc0c2msh426ad7c3fd273afp1ee14cjsn1c38800d02f7",
+      "x-rapidapi-key": "",
     },
   });
   return data;
